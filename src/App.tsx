1352692a6d7a78ba8faf35ffecb9c1e29fbe14c3
Burhan Syed <busyed98@gmail.com>
2024-07-16 14:48:46 -0400

    init shadcn + tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <main className='mx-auto max-w-5xl p-4'>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +27,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </main>
   )
 }
 
